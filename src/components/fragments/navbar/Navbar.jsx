2e8c1b7475ac8e8c7b868f1c35365c036858e932
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-07-02 17:26:11 +0700

    styled navbar

--- src/components/fragments/navbar/Navbar.jsx
+++ src/components/fragments/navbar/Navbar.jsx
@@ -9,82 +9,97 @@ import {
   Button,
   Menu,
   MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
   useDisclosure,
   useColorModeValue,
   Stack,
 } from '@chakra-ui/react';
 import {AlignCenter, X} from 'react-feather'
+import {NavLink} from 'react-router-dom'
 import Logo from '../../../assets/image/logo.svg'
 
-const Links = ['Dashboard', 'Projects', 'Team'];
+const Links = ['Beranda', 'Info', 'Dukungan']
 
-const NavLink = ({ children }) => (
+const NavbarLink = ({ children, href }) => (
   <Link
     px={2}
     py={1}
     rounded={'md'}
     _hover={{
       textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
+      textColor: 'blue.700'
     }}
-    href={'#'}>
+    href={href}>
     {children}
   </Link>
 );
 
+let getJson = localStorage.getItem('logged')
+const NavbarLoginMenu = () => {
+  if(getJson === null) {
+    return(
+      <NavLink to='/login'>
+        <Button>Login</Button>
+      </NavLink>
+    )
+  }else {
+    return(
+      <Menu>
+        <MenuButton
+          as={Button}
+          rounded={'full'}
+          variant={'link'}
+          cursor={'pointer'}
+          minW={0}>
+          <Avatar
+            size={'sm'}
+            src={
+              'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+            }
+          />
+        </MenuButton>              
+      </Menu>
+    )
+  }
+}
+
 export default function Navbar() {
-    const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-      <Box bg={useColorModeValue('white')} px={4}>
+      <Box bg={useColorModeValue('white')} px={[4,20]}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
           <IconButton
             size={'md'}
+            p={2}
             icon={isOpen ? <X /> : <AlignCenter />}
             aria-label={'Open Menu'}
             display={{ md: 'none' }}
             onClick={isOpen ? onClose : onOpen}
           />
           <HStack spacing={8} alignItems={'center'}>
-            <Box w={'28'}><img src={Logo}/></Box>
-            <HStack
+            <Box w={'28'}><img src={Logo}/></Box>            
+          </HStack>
+          <HStack
               as={'nav'}
-              spacing={4}
+              spacing={10}
+              fontWeight={'bold'}
+              fontSize={'lg'}
+              textColor={'gray.500'}
               display={{ base: 'none', md: 'flex' }}>
               {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+                <NavbarLink key={link} href={`/${link}`}>{link}</NavbarLink>
               ))}
             </HStack>
-          </HStack>
           <Flex alignItems={'center'}>
-           <Link to='/login'><Button>Login</Button></Link>
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
-              </MenuButton>              
-            </Menu>
+            {NavbarLoginMenu()}
           </Flex>
         </Flex>
-
         {isOpen ? (
           <Box pb={4} display={{ md: 'none' }}>
             <Stack as={'nav'} spacing={4}>
               {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+                <NavbarLink key={link} href={`/${link}`}>{link}</NavbarLink>
               ))}
             </Stack>
           </Box>
