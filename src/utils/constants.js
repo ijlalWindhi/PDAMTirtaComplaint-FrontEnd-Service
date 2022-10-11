d432058e13637af240b5d9cd141495abe9fae579
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-10-11 19:56:10 +0700

    setup for vercel route

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:2004/";
+const API_URL = "https://pdam-tirta-sidoarjo.herokuapp.com/";
 
-const URL_Image = "http://localhost:2004/image/";
+const URL_Image = "https://pdam-tirta-sidoarjo.herokuapp.com/image/";
 
 export { API_URL, URL_Image };
